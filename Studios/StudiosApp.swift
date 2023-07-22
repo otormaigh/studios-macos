0e067ebb0d81ae48c45c44ec4891552194afff51
Elliot Tormey <elliot.tormey@gmail.com>
2023-07-22 18:29:38 +0100

    Initial Commit

---
+++ Studios/StudiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudiosApp.swift
+//  Studios
+//
+//  Created by Fred on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
